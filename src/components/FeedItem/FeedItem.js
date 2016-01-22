d91c247109e87d870f80303865631c0ba77e4ceb
brennanerbz <brennan.erbeznik@gmail.com>
2016-01-21 18:51:52 -0800

    added unread style for feed

--- src/components/FeedItem/FeedItem.js
+++ src/components/FeedItem/FeedItem.js
@@ -8,7 +8,12 @@ export default class FeedItem extends Component {
 		const { index } = this.props,
 		style = require('./FeedItem.scss');
 		return (
-			<li className={'feed' + index + ' ' + style.feed + ' ' + (index == 0 ? style.active : '')}>
+			<li 
+			className={'feed' + 
+			index + ' ' + 
+			style.feed + ' ' + 
+			(index == 0 ? style.active : '')
+			+ (index == 1 ? style.unread : '')}>
 				<a className={style.feed_name}>
 					<span className="overflow_ellipsis">
 						<span className={style.prefix_icon}>#</span>
